8943bc24076752cf70058c527504395946afd765
Iryna1V <ivhaievska@gmail.com>
2025-11-05 17:00:24 -0500

    changing

--- src/Buttons.jsx
+++ src/Buttons.jsx
@@ -1,20 +1,27 @@
 import { data } from "./data"
-function Buttons ({filteredClothes}) {
-  const filteredClothes = (searchTerm) => {
-    const newClothesArray = data.filter(item => item.searchTerm.includes(searchTerm))
-    setClothes(newClothesArray)
-  }
-  )
+function Buttons({ setClothes }) {
+const handleFilter = (searchTerm) => {
+  const newClothesArray = data.filter(item => 
+    item.searchTerm.includes(searchTerm)
+  );
+    setClothes(newClothesArray);
+};
+
+// function Buttons ({filteredClothes, setClothes}) {
+  // const filteredClothes = (searchTerm) => {
+  //   const newClothesArray = data.filter(item => item.searchTerm.includes(searchTerm));
+  
   return (
     <div className="cont">
-      <button className="change" onClick={() => setProducts(dataShop)}>Show all</button>
-      <button className="change" onClick={() => filteredClothes("dress")}>Dresses</button>
-      <button className="change" onClick={() => filteredClothes("pants")}>Pants</button>
-      <button className="change" onClick={() => filteredClothes("skirt")}>Skirts</button>
-      <button className="change" onClick={() => filteredClothes("shoes")}>Shoes</button>
-      <button className="change" onClick={() => filteredClothes("shirt")}>Shirts</button>
+      <button className="change" onClick={() => setClothes(data)}>Show all</button>
+      <button className="change" onClick={() => handleFilter("man")}>Man</button>
+      <button className="change" onClick={() => handleFilter("woman")}>Woman</button>
+      <button className="change" onClick={() => handleFilter("dress")}>Dresses</button>
+      <button className="change" onClick={() => handleFilter("pants")}>Pants</button>
+      <button className="change" onClick={() => handleFilter("skirt")}>Skirts</button>
+      <button className="change" onClick={() => handleFilter("shoes")}>Shoes</button>
+      <button className="change" onClick={() => handleFilter("shirt")}>Shirts</button>
     </div>
   );
 }
-
 export default Buttons;
--- src/Home.jsx
+++ src/Home.jsx
@@ -5,19 +5,19 @@ import Buttons from "./Buttons";
 
 function Home() {
   const [clothes, setClothes] = useState(data);
-  const chosenClothes = (searchTerm) => {
-    const newClothes = data.filter(
-      (element) => element.searchTerm === searchTerm
-    );
-    setClothes(newClothes);
-  };
+  // const chosenClothes = (searchTerm) => {
+  //   const newClothes = data.filter(
+  //     (element) => element.searchTerm === searchTerm
+  //   );
+  //   setClothes(newClothes);
+  // };
   return (
     <div>
       <div className="cont">
         <h2 className="back"> Free Standard Shipping</h2>
       </div>
-      <Buttons setClothes={setClothes} />
-      <Buttons filteredClothes={chosenClothes} />
+      <Buttons setClothes={setClothes}/>
+      {/* <Buttons filteredClothes={chosenClothes} /> */}
       <Clothes itemsForSale={clothes} />
     </div>
   );
